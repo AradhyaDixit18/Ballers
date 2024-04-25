81db78217e1604ed656a4719fddd7988850073b5
Chirag Kumar <chiragkumar77777@gmail.com>
2024-04-25 15:39:20 +0530

    Eyes

--- src/Components/Eyes.jsx
+++ src/Components/Eyes.jsx
@@ -18,9 +18,10 @@ function Eyes() {
 	});
 
 	return (
-		<div className="eyes w-full h-screen overflow-hidden">
+		<div className="eyes w-full h-screen overflow-hidden bg-[#f6f6f6]">
 			<div
 				data-scroll
+				data-scroll-section
 				data-scroll-speed="-.7"
 				className='relative w-full h-full bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg")]'
 			>
